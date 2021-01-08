013ec97d8568ce35829f32bc975e920b25f531dc
tmd9936 <tmd9936@gmail.com>
2021-01-08 09:34:27 +0900

    include 세팅

--- src/pub.cpp
+++ src/pub.cpp
@@ -3,12 +3,16 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <sstream> // for converting the command line parameter to integer
+#include "linefinder.h"
+#include "IPM.h"
+
 
 int main(int argc, char** argv)
 {
   // Check if video source has been passed as a parameter
-  if(argv[1] == NULL) return 1;
-
+  int video_source = 0;
+  // if(argv[1] == NULL) return 1;
+	
   ros::init(argc, argv, "image_publisher");
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
@@ -24,7 +28,7 @@ int main(int argc, char** argv)
   // cap setting
   cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
   cap.set(CV_CAP_PROP_FRAME_WIDTH, 480);
-  cap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('M', 'J', 'P', 'G'));
+  cap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('D', 'I', 'V', 'X'));
   
   // Check if video device can be opened with the given index
   if(!cap.isOpened()) return 1;
@@ -35,6 +39,7 @@ int main(int argc, char** argv)
   while (nh.ok()) {
     cap >> frame;
     // Check if grabbed frame is actually full with some content
+
     if(!frame.empty()) {
       msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
       pub.publish(msg);
