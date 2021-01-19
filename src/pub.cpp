f699093f21be95bd7057b0054c28290d16633c31
tmd9936 <you@example.com>
2021-01-19 17:22:40 +0900

    modified:   msg/TrafficState.msg 	modified:   src/pub.cpp

--- src/pub.cpp
+++ src/pub.cpp
@@ -37,6 +37,8 @@
 #define RED 0
 #define GREEN 1
 
+#define CENTER_VALUE 479
+
 using namespace std;
 using namespace cv;
 
@@ -63,6 +65,21 @@ double avr_center_to_right = 0;
 double center_to_right = -1;
 double center_to_left = -1;
 
+// 선이 1개 일 때의 변수
+int top_x = 0;
+int top_y = 0;
+int bottom_x = 0;
+int bottom_y = 0;
+int center_x = 0;
+int center_y = 0;
+double line_degree = 0.0;
+double y_intercept = 0.0;
+Point2i pre_center_point;
+int chg_amount = 0;
+
+int pass_chg_amount_value = 120;
+
+//
 int centerline = 0;
 
 int diff = 0;
@@ -72,7 +89,13 @@ int degree = 0;
 int counter = 0;
 int move_mouse_pixel = 0;
 
-int is_two_line = 0;
+int line_count = 0;
+int pre_line_count = 0;
+
+void initParams(ros::NodeHandle *nh_priv)
+{
+	nh_priv->param("pass_chg_amount_value", pass_chg_amount_value, pass_chg_amount_value);
+}
 
 int main(int argc, char **argv)
 {
@@ -88,6 +111,8 @@ int main(int argc, char **argv)
 
 	// 데이터 전송용 퍼블리셔
 	ros::Publisher traffic_pub = nh.advertise<camera_opencv::TrafficState>("traffic_state", 1);
+	ros::NodeHandle nh_priv{"~"};
+	initParams(&nh_priv);
 
 	camera_opencv::TrafficState traffic_state_msg;
 	traffic_state_msg.line_state = 0;
@@ -113,10 +138,14 @@ int main(int argc, char **argv)
 	cv::UMat gray, blur, sobel;
 	cv::Mat contours;
 
+	// 왼쪽 오른쪽 구역 직선의 방성식을 얻기위한 mat 데이터
+	cv::Mat slice_line_mat = cv::Mat::zeros(height, width, CV_8SC1);
+
 	// 주차구역 검출용 mat 데이터
 	cv::Mat frame_hsv;
 	cv::Mat red_mask, red_frame;
 	cv::Mat red_image;
+	pre_center_point = Point2i(0, 0);
 
 	vector<vector<Point>> rect_cont;
 	vector<Point2f> approx;
@@ -126,7 +155,6 @@ int main(int argc, char **argv)
 	Scalar upper_red = Scalar(179, 255, 255);
 
 	///////
-
 	IPM ipm;
 	LineFinder ld;
 
@@ -154,14 +182,18 @@ int main(int argc, char **argv)
 	while (nh.ok())
 	{
 		cap >> frame;
-		//cv::imshow("frame", frame);
+		cv::imshow("frame", frame);
 		ipm.applyHomography(frame, outputFrame);
+
+		ROS_INFO("pass_chg_amount_value = %d", pass_chg_amount_value);
 
 		// ---------  선분 검출 -------------
 
 		//cv::resize(outputFrame, outputFrame, cv::Size(640, 480));
 		outputFrame = outputFrame(Range::all(), Range(80, width));
-		//cv::imshow("outputFrame", outputFrame);
+
+		// 560, 480
+		cv::imshow("outputFrame", outputFrame);
 
 		cv::cvtColor(outputFrame, gray, COLOR_RGB2GRAY);
 		cv::blur(gray, blur, cv::Size(15, 15));
@@ -170,7 +202,7 @@ int main(int argc, char **argv)
 
 		ld.setLineLengthAndGap(20, 120);
 		ld.setMinVote(55);
-		std::vector<cv::Vec4i> li = ld.findLines(contours);
+		std::vector<cv::Vec4i> lines = ld.findLines(contours);
 		ld.drawDetectedLines(contours);
 
 		cv::imshow("contours", contours);
@@ -183,10 +215,9 @@ int main(int argc, char **argv)
 		//morphological closing 영역의 구멍 메우기
 		dilate(contours, contours, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 		dilate(contours, contours, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
+		
 		// ---------  자율주행용 차선의 라디안 추출  -------------
-
-		bottom_center = width/2 - 50;
+		bottom_center = width/2 - 40;
 		sum_centerline = 0;
 		count_centerline = 0;
 		first_centerline = 0;
@@ -195,104 +226,155 @@ int main(int argc, char **argv)
 		avr_center_to_right = 0;
 		degree = 0;
 		diff = 0;
+		
+		// 선이 1개 일 때의 변수
+		top_x = -1;
+		top_y = 0;
+		bottom_x = -1;
+		bottom_y = height;
+		center_x = 0;
+		center_y = height/2;
+		line_degree = 0.0;
+		y_intercept = 0.0;
 
-		//#pragma omp parallel for
-		for (int i = height; i > 0; i--)
+		//ROS_INFO("lines.size() %d", lines.size());
+		// line_size()가 5개 이하면 라인이 없다고 판단
+		if (lines.size() < 6)
 		{
-			center_to_right = -1;
-			center_to_left = -1;
-
-			for (int j = 0; j < width/2; j++)
-			{
-				if (contours.at<uchar>(i, bottom_center + j) == 112 && center_to_right == -1)
-				{
-					center_to_right = j;
-				}
-				if (contours.at<uchar>(i, bottom_center - j) == 112 && center_to_left == -1)
-				{
-					center_to_left = j;
-				}
-			}
-			if (center_to_left != -1 && center_to_right != -1)
-			{
-				centerline = (center_to_right - center_to_left + 2 * bottom_center) / 2;
-				if (first_centerline == 0)
-				{
-					first_centerline = centerline;
-				}
-				sum_centerline += centerline;
-				avr_center_to_left = (avr_center_to_left * count_centerline + center_to_left) / count_centerline + 1;
-				avr_center_to_right = (avr_center_to_right * count_centerline + center_to_right) / count_centerline + 1;
-				last_centerline = centerline;
-				count_centerline++;
-
-			}
-			else
-			{
-			}
-		}
-
-				
-
-		if (count_centerline != 0)
-		{
-			diff = sum_centerline / count_centerline - bottom_center;
-			degree = atan2(last_centerline - first_centerline, count_centerline) * 180 / PI;
-			move_mouse_pixel = 0 - counter + diff;
-
-			//ROS_INFO("move_mouse_pixel = %d", move_mouse_pixel);
-			//ROS_INFO("degree msg = %d", degree);
-
-			counter = diff;
-
-			if(move_mouse_pixel == 0){
-				if(IPM_diff > 0){
-					IPM_RIGHT -= 1;
-					IPM_LEFT -= 1;
-					IPM_diff -= 1;
-				}
-				else if(IPM_diff < 0){
-					IPM_RIGHT += 1;
-					IPM_LEFT += 1;
-					IPM_diff += 1;
-				}
-				else{
-					IPM_RIGHT = width/2+380;
-					IPM_LEFT = width/2-380;
-					IPM_diff = 0;
-				}
-			}
-			else{
-				if (IPM_diff >= -30 && IPM_diff <= 30){
-					IPM_RIGHT += move_mouse_pixel;
-					IPM_LEFT += move_mouse_pixel;
-					if(move_mouse_pixel > 0){
-						IPM_diff++;
-					}
-					else{
-						IPM_diff--;
-					}
-				}
-			}
-		}
-		//ROS_INFO("diff %d", int(diff));
-		//ROS_INFO("move_mouse_pixel %d", int(move_mouse_pixel));
-		ROS_INFO("count_centerline %d", count_centerline);
-
-		//ROS_INFO("li.size %d", li.size());
-
-
-		//traffic_state_msg.line_state = degree;
-		traffic_state_msg.line_state = diff;
-		if(count_centerline == 0 )
-		{
-			traffic_state_msg.is_two_line = 0;
+			line_count = 0;
 		}
 		else
 		{
-			traffic_state_msg.is_two_line = 1;
+			// 중앙 선분 체크
+			for (int i = height; i > 0; i--)
+			{
+				center_to_right = -1;
+				center_to_left = -1;
+
+				for (int j = 0; j < width/2; j++)
+				{
+					if (contours.at<uchar>(i, bottom_center + j) == 112 && center_to_right == -1)
+					{
+						center_to_right = j;
+						//ROS_INFO("center_to_right %d", j);
+					}
+					if (contours.at<uchar>(i, bottom_center - j) == 112 && center_to_left == -1)
+					{
+						center_to_left = j;
+						//ROS_INFO("center_to_left %d", j);
+					}
+				}
+				if (center_to_left != -1 && center_to_right != -1)
+				{
+					centerline = (center_to_right - center_to_left + 2 * bottom_center) / 2;
+					if (first_centerline == 0)
+					{
+						first_centerline = centerline;
+					}
+					sum_centerline += centerline;
+					avr_center_to_left = (avr_center_to_left * count_centerline + center_to_left) / count_centerline + 1;
+					avr_center_to_right = (avr_center_to_right * count_centerline + center_to_right) / count_centerline + 1;
+					last_centerline = centerline;
+					count_centerline++;
+
+				}
+				else
+				{
+				}
+			}
+			
+			// 선분이 2개 일 때
+			//if (count_centerline > 400 && count_centerline < 500)
+			if (count_centerline == CENTER_VALUE && lines.size() > 23)
+			{
+				diff = sum_centerline / count_centerline - bottom_center;
+				//degree = atan2(last_centerline - first_centerline, count_centerline) * 180 / PI;
+				move_mouse_pixel = 0 - counter + diff;
+
+				counter = diff;
+				line_count = 2;
+				degree = 0;
+			}
+			// 선분이 1개 일 때
+			else if(count_centerline == 0 || centerline == 280)
+			{
+				// 위에 좌표, 아래좌표 얻기
+				for (int i = 0; i < width; i++)
+				{
+					// 위에 좌표
+					if (contours.at<uchar>(0, i) == 112)
+					{
+						top_x = i;
+						//ROS_INFO("top_x %d", i);
+					}
+					// 아래 좌표
+					if (contours.at<uchar>(height-5, i) == 112)
+					{
+						bottom_x = i;
+						//ROS_INFO("bottom_x %d", i);
+					}
+				}
+				// 기울기 구하기
+				//line_degree = (height-5) / ((double)bottom_x - (double)top_x);
+
+				// y절편 구하기
+				//y_intercept = -(line_degree * top_x);
+				
+				// 중앙점 좌표구하기
+				//one_line_center.x = (line_degree*one_line_center.y) + y_intercept;
+				if (bottom_x != -1 && top_x != -1)
+				{
+					// 중앙점의 x좌표 구하기
+					center_x = (bottom_x + top_x) / 2;
+
+					//ROS_INFO("center_x = %d", center_x);
+
+					if (pre_line_count == 1 || pre_line_count == 2)
+					{
+						pre_center_point.y = height/2;
+						pre_center_point.x = center_x;
+					}
+
+					chg_amount = pre_center_point.x - center_x;
+
+					if(abs(chg_amount) < pass_chg_amount_value)
+					{
+						degree = 0;
+					}
+					else
+					{
+						if(chg_amount > 0)
+						{
+							degree = 1;
+						}
+						else
+						{
+							degree = 0;
+						}
+						
+					}
+
+					line_count = 1;
+				}
+				//ROS_INFO("y_intercept = %f", y_intercept);
+				//ROS_INFO("one_line_center.x = %d", one_line_center.x);
+			}
+			else {}
+			//ROS_INFO("diff %d", int(diff));
+			//ROS_INFO("move_mouse_pixel %d", int(move_mouse_pixel));
+			//ROS_INFO("degree %d", int(degree));
 		}
-		
+
+		pre_line_count = line_count;
+
+		//ROS_INFO("count_centerline %d", count_centerline);
+		//ROS_INFO("centerline %d", centerline);
+
+		// 라인 관련 메세지(라인 각도, 라인 수)
+		//traffic_state_msg.line_state = degree;
+		traffic_state_msg.line_state = degree;
+		traffic_state_msg.line_count = line_count;
+
 
 		// ---------  주차 구역 검출  -------------
 		cv::cvtColor(outputFrame, frame_hsv, COLOR_BGR2HSV);
@@ -309,45 +391,61 @@ int main(int argc, char **argv)
 
 		//cv::bitwise_and(outputFrame, outputFrame, red_image, red_mask);
 
-		//cv::imshow("red_image", red_image);
+		//cv::imshow("red_mask", red_mask);
 
 		// 사각형 검출
 		cv::findContours(red_mask, rect_cont, RETR_LIST, CHAIN_APPROX_SIMPLE);
 		approx_size = 0;
 
-		for (size_t i = 0; i < rect_cont.size(); i++)
+		//ROS_INFO("rect_cont.size() %d", rect_cont.size());
+
+		if (rect_cont.size() == 1)
 		{
-			approxPolyDP(Mat(rect_cont[i]), approx, arcLength(Mat(rect_cont[i]), true) * 0.02, true);
-
-			if (fabs(contourArea(Mat(approx))) > 100)
+			for (size_t i = 0; i < rect_cont.size(); i++)
 			{
-				approx_size = approx.size();
+				approxPolyDP(Mat(rect_cont[i]), approx, arcLength(Mat(rect_cont[i]), true) * 0.02, true);
 
-				if (approx_size % 2 == 0)
+				if (fabs(contourArea(Mat(approx))) > 100)
 				{
-					line(red_mask, approx[0], approx[approx.size() - 1], Scalar(0, 255, 0), 3);
+					approx_size = approx.size();
 
-					for (int k = 0; k < approx_size - 1; k++)
-						line(red_mask, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
+					if (approx_size % 2 == 0)
+					{
+						line(red_mask, approx[0], approx[approx.size() - 1], Scalar(0, 255, 0), 3);
 
-					for (int k = 0; k < approx_size; k++)
-						circle(red_mask, approx[k], 3, Scalar(0, 0, 255));
-				}
-				else
-				{
-					line(red_mask, approx[0], approx[approx.size() - 1], Scalar(0, 255, 0), 3);
+						for (int k = 0; k < approx_size - 1; k++)
+							line(red_mask, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
 
-					for (int k = 0; k < approx_size - 1; k++)
-						line(red_mask, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
+						for (int k = 0; k < approx_size; k++)
+							circle(red_mask, approx[k], 3, Scalar(0, 0, 255));
+					}
+					else
+					{
+						line(red_mask, approx[0], approx[approx.size() - 1], Scalar(0, 255, 0), 3);
 
-					for (int k = 0; k < approx_size; k++)
-						circle(red_mask, approx[k], 3, Scalar(0, 0, 255));
-				}
+						for (int k = 0; k < approx_size - 1; k++)
+							line(red_mask, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
 
-				// 꼭지점의 개수가 4개일때 출력
-				//if(size == 4 && isContourConvex(Mat(approx)))
-			}
-		}
+						for (int k = 0; k < approx_size; k++)
+							circle(red_mask, approx[k], 3, Scalar(0, 0, 255));
+					}
+
+					// 꼭지점의 개수가 4개일때 출력
+					if(approx_size == 4 && isContourConvex(Mat(approx)))
+					{
+						approx_size = 4;
+						//ROS_INFO("rect_cont[%d][0].x %d", i, rect_cont[i][0].x);
+						//ROS_INFO("rect_cont[%d][0].y %d", i, rect_cont[i][0].y);
+					}
+					else 
+					{
+						approx_size = 0;
+					}
+				} //(fabs(contourArea(Mat(approx))) > 100)
+			} // for (size_t i = 0; i < rect_cont.size(); i++)
+		}//(rect_cont.size() == 1)
+
+		traffic_state_msg.station_area = approx_size;
 
 		// ---------  신호등 색 검출  -------------
 		Mat frame_light;
@@ -432,8 +530,6 @@ int main(int argc, char **argv)
 		// -------------------------------------
 
 		//imshow("traffic_light", frame_light);
-
-		traffic_state_msg.station_area = approx_size;
 
 		//traffic_state_msg.traffic_color = 1;
 
