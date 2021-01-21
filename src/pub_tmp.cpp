cf03adf98af66a818f794c3070fc4161b96f6ac2
tmd9936 <you@example.com>
2021-01-21 09:25:07 +0900

    new file:   src/lane_keeping.py 	modified:   src/pub.cpp 	new file:   src/pub_tmp.cpp

--- src/pub_tmp.cpp
+++ src/pub_tmp.cpp
@@ -54,13 +54,34 @@ int IPM_diff = 0;
 vector<Point2f> origPoints;
 vector<Point2f> dstPoints;
 
-int pass_chg_amount_value = 120;
+int bottom_center = 320;
+int sum_centerline = 0;
+int count_centerline = 0;
+int first_centerline = 0;
+int last_centerline = 0;
+double avr_center_to_left = 0;
+double avr_center_to_right = 0;
 
-// 선검출
-int deviation = 0;
+double center_to_right = -1;
+double center_to_left = -1;
+
+// 선이 1개 일 때의 변수
+int top_x = 0;
+int top_y = 0;
+int bottom_x = 0;
+int bottom_y = 0;
+int center_x = 0;
+int center_y = 0;
+double line_degree = 0.0;
+double y_intercept = 0.0;
+Point2i pre_center_point;
+int chg_amount = 0;
+
+int pass_chg_amount_value = 120;
 
 //
 int centerline = 0;
+
 int diff = 0;
 
 int degree = 0;
@@ -70,9 +91,6 @@ int move_mouse_pixel = 0;
 
 int line_count = 0;
 int pre_line_count = 0;
-
-bool left_push = false;
-bool right_push = false;
 
 void initParams(ros::NodeHandle *nh_priv)
 {
@@ -109,22 +127,16 @@ int main(int argc, char **argv)
 	cv::VideoCapture cap(video_source);
 	// cap setting
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, width);
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT, height);
-	//cap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('D', 'I', 'V', 'X'));
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, height);
+	cap.set(CV_CAP_PROP_FOURCC, CV_FOURCC('D', 'I', 'V', 'X'));
 
 	// Check if video device can be opened with the given index
 	if (!cap.isOpened())
 		return 1;
-	// 선분검출용
-	cv::Mat frame, outputFrame, view_frame, new_frame ,canny;
+	// 선분검출용 mat 데이터
+	cv::Mat frame, outputFrame;
 	cv::UMat gray, blur, sobel;
-	cv::Mat contours, region_mask, edges_mask;
-
-	vector<Point> polygon;
-	polygon.push_back(Point(0, height));
-	polygon.push_back(Point(0,  height/2));
-	polygon.push_back(Point(width , height/2));
-	polygon.push_back(Point(width , height));
+	cv::Mat contours;
 
 	// 왼쪽 오른쪽 구역 직선의 방성식을 얻기위한 mat 데이터
 	cv::Mat slice_line_mat = cv::Mat::zeros(height, width, CV_8SC1);
@@ -133,6 +145,7 @@ int main(int argc, char **argv)
 	cv::Mat frame_hsv;
 	cv::Mat red_mask, red_frame;
 	cv::Mat red_image;
+	pre_center_point = Point2i(0, 0);
 
 	vector<vector<Point>> rect_cont;
 	vector<Point2f> approx;
@@ -169,232 +182,202 @@ int main(int argc, char **argv)
 	while (nh.ok())
 	{
 		cap >> frame;
+		//cv::imshow("frame", frame);
+		ipm.applyHomography(frame, outputFrame);
 
-		frame.copyTo(view_frame);
+		ROS_INFO("pass_chg_amount_value = %d", pass_chg_amount_value);
 
 		// ---------  선분 검출 -------------
 
-		// detect_edges
-		cv::cvtColor(frame, gray, COLOR_BGR2GRAY);
+		//cv::resize(outputFrame, outputFrame, cv::Size(640, 480));
+		outputFrame = outputFrame(Range::all(), Range(80, width));
+
+		// 560, 480
+		//cv::imshow("outputFrame", outputFrame);
+
+		cv::cvtColor(outputFrame, gray, COLOR_RGB2GRAY);
 		cv::blur(gray, blur, cv::Size(15, 15));
-
-		//cv::cvtColor(frame, blur, COLOR_BGR2HSV);
-		
-		// cv::inRange(blur, Scalar(0,0,0,0), Scalar(180, 255, 80, 0), edges_mask);
-	
-		// cv::Canny(edges_mask, contours, 50, 100);
-
 		cv::Sobel(blur, sobel, blur.depth(), 1, 0, 3, 0.5, 127);
 		cv::threshold(sobel, contours, 145, 255, CV_THRESH_BINARY);
 
-		//morphological closing 영역의 구멍 메우기
-		dilate(contours, contours, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		ld.setLineLengthAndGap(20, 120);
+		ld.setMinVote(55);
+		std::vector<cv::Vec4i> lines = ld.findLines(contours);
+		ld.drawDetectedLines(contours);
+
+		//cv::imshow("contours", contours);
+
+		//morphological opening 작은 점들을 제거
+		erode(contours, contours, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(contours, contours, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 		erode(contours, contours, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-		// ld.setLineLengthAndGap(20, 120);
-		// ld.setMinVote(55);
-		// vector<cv::Vec4i> lines = ld.findLines(contours);
-
-		// cv::cvtColor(contours, new_frame, cv::COLOR_GRAY2BGR);
-		//ld.drawDetectedLines(new_frame, Scalar(255, 0, 0));
-
-		// cv::cvtColor(new_frame, new_frame, cv::COLOR_BGR2HSV);
-
-		// // cv::inRange()
-		// dilate(new_frame, new_frame, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		// //erode(new_frame, new_frame, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-		// cv::inRange(new_frame, Scalar(90, 120, 0), Scalar(150, 255, 255), edges_mask);
-		// cv::Canny(edges_mask, canny, 50, 100);
-
-		//region_of_interest
-		region_mask = Mat::zeros(height, width, 0);
-		cv::fillConvexPoly(region_mask, polygon, 255);	
-		cv::bitwise_and(contours, region_mask, contours);
-
-		// detect_line_segments
-		vector<cv::Vec4i> line_segments;
-		cv::HoughLinesP(contours, line_segments, 1, CV_PI / 180.0, 55, 5, 150);
-
-		// average_slope_intercept
-		vector<Vec4i> lane_lines;
-
-		vector<Point2f> left_fit;
-		vector<Point2f> right_fit;
+		//morphological closing 영역의 구멍 메우기
+		dilate(contours, contours, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(contours, contours, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 		
-		double boundary = 0.3;
-		double left_region_boundary = width * (1.0 - boundary);
-		double right_region_boundary = width * (boundary);
-
-
-		for(auto &line_segment : line_segments)
-		{
-			
-			int x1 = line_segment[0];
-			int y1 = line_segment[1];
-			int x2 = line_segment[2];
-			int y2 = line_segment[3];
-
-			if (x1 == x2)
-				continue;
-
-			double slope = (double(y2 - y1)) / (double(x2 - x1));
-			double intercept = y1 - (slope * x1);
-
-			if (slope < 0.)
-			{
-				if (x1 < left_region_boundary && x2 < left_region_boundary)
-				{
-					left_fit.push_back(Point2f(slope, intercept));
-					left_push = true;
-				}
-			}
-			else
-			{
-				if(x1 > right_region_boundary && x2 > right_region_boundary)
-				{
-					right_fit.push_back(Point2f(slope, intercept));
-					right_push = true;
-				}
-			}
-
-			
-		}
-
-		Point2f left_fit_average = Point2f(0., 0.);
-		for(auto &fit : left_fit)
-		{
-			left_fit_average.x += fit.x;
-			left_fit_average.y += fit.y;
-		}
-		left_fit_average.x /= float(left_fit.size());
-		left_fit_average.y /= float(left_fit.size());
-
-		if (left_fit.size() > 0)
-		{
-			float slope = left_fit_average.x;
-			float intercept = left_fit_average.y;
-			
-			int y1 = height;
-			int y2 = int(y1 / 2.0);
+		// ---------  자율주행용 차선의 라디안 추출  -------------
+		bottom_center = width/2 - 40;
+		sum_centerline = 0;
+		count_centerline = 0;
+		first_centerline = 0;
+		last_centerline = 0;
+		avr_center_to_left = 0;
+		avr_center_to_right = 0;
+		degree = 0;
+		diff = 0;
 		
-			if (slope == 0)
-				slope = 0.1;
+		// 선이 1개 일 때의 변수
+		top_x = -1;
+		top_y = 0;
+		bottom_x = -1;
+		bottom_y = height;
+		center_x = 0;
+		center_y = height/2;
+		line_degree = 0.0;
+		y_intercept = 0.0;
 
-			int x1 = int((y1 - intercept) / slope);
-			int x2 = int((y2 - intercept) / slope);
-
-			lane_lines.push_back(cv::Vec4i(x1, y1, x2, y2));
-
-		}
-
-		Point2f right_fit_average = Point2f(0., 0.);
-		for(auto &fit : right_fit)
+		//ROS_INFO("lines.size() %d", lines.size());
+		// line_size()가 5개 이하면 라인이 없다고 판단
+		if (lines.size() < 6)
 		{
-			right_fit_average.x += fit.x;
-			right_fit_average.y += fit.y;
-		}
-		right_fit_average.x /= right_fit.size();
-		right_fit_average.y /= right_fit.size();
-
-		if (right_fit.size() > 0)
-		{
-			float slope = right_fit_average.x;
-			float intercept = right_fit_average.y;
-			
-			int y1 = height;
-			int y2 = int(y1 / 2);
-
-			if (slope == 0)
-				slope = 0.1;
-
-			int x1 = int((y1 - intercept) / slope);
-			int x2 = int((y2 - intercept) / slope);
-
-			lane_lines.push_back(cv::Vec4i(x1, y1, x2, y2));
-		}
-
-		// disply lines
-		Mat line_image = Mat::zeros(height, width, frame.type());
-
-		if (lane_lines.size() != 0)
-		{
-			for(auto &line : lane_lines)
-			{
-				int x1 = line[0];
-				int y1 = line[1];
-				int x2 = line[2];
-				int y2 = line[3];
-
-				cv::line(line_image, Point(x1, y1), Point(x2, y2), Scalar(125,125,125), 6);
-			}
-		}
-		
-		cv::addWeighted(view_frame, 0.8, line_image, 1, 1, view_frame);
-
-		// get_steering_angle
-		double x_offset = 0.;
-		double y_offset = 0.;
-
-		if(lane_lines.size() == 2)
-		{
-			int left_x2 = lane_lines[0][2];
-			int right_x2 = lane_lines[1][2];
-			int mid = int(width / 2.0);
-			x_offset = (left_x2 + right_x2) / 2.0 - mid;
-			y_offset = int(height / 2.0);
-
-		}
-		else if (lane_lines.size() == 1)
-		{
-			int x1 = lane_lines[0][0];
-			int x2 = lane_lines[0][2];
-			x_offset = x2 - x1;
-			y_offset = int(height / 2.0);
-		}
-		else if (lane_lines.size() == 0)
-		{
-			x_offset = 0;
-			y_offset = int(height / 2.0);
+			line_count = 0;
 		}
 		else
 		{
-			x_offset = 0;
-			y_offset = int(height / 2.0);
+			// 중앙 선분 체크
+			for (int i = height; i > 0; i--)
+			{
+				center_to_right = -1;
+				center_to_left = -1;
+
+				for (int j = 0; j < width/2; j++)
+				{
+					if (contours.at<uchar>(i, bottom_center + j) == 112 && center_to_right == -1)
+					{
+						center_to_right = j;
+						//ROS_INFO("center_to_right %d", j);
+					}
+					if (contours.at<uchar>(i, bottom_center - j) == 112 && center_to_left == -1)
+					{
+						center_to_left = j;
+						//ROS_INFO("center_to_left %d", j);
+					}
+				}
+				if (center_to_left != -1 && center_to_right != -1)
+				{
+					centerline = (center_to_right - center_to_left + 2 * bottom_center) / 2;
+					if (first_centerline == 0)
+					{
+						first_centerline = centerline;
+					}
+					sum_centerline += centerline;
+					avr_center_to_left = (avr_center_to_left * count_centerline + center_to_left) / count_centerline + 1;
+					avr_center_to_right = (avr_center_to_right * count_centerline + center_to_right) / count_centerline + 1;
+					last_centerline = centerline;
+					count_centerline++;
+
+				}
+				else
+				{
+				}
+			}
+			
+			// 선분이 2개 일 때
+			//if (count_centerline > 400 && count_centerline < 500)
+			if (count_centerline == CENTER_VALUE && lines.size() > 23)
+			{
+				diff = sum_centerline / count_centerline - bottom_center;
+				//degree = atan2(last_centerline - first_centerline, count_centerline) * 180 / PI;
+				move_mouse_pixel = 0 - counter + diff;
+
+				counter = diff;
+				line_count = 2;
+				degree = 0;
+			}
+			// 선분이 1개 일 때
+			else if(count_centerline == 0 || centerline == 280)
+			{
+				// 위에 좌표, 아래좌표 얻기
+				for (int i = 0; i < width; i++)
+				{
+					// 위에 좌표
+					if (contours.at<uchar>(0, i) == 112)
+					{
+						top_x = i;
+						//ROS_INFO("top_x %d", i);
+					}
+					// 아래 좌표
+					if (contours.at<uchar>(height-5, i) == 112)
+					{
+						bottom_x = i;
+						//ROS_INFO("bottom_x %d", i);
+					}
+				}
+				// 기울기 구하기
+				//line_degree = (height-5) / ((double)bottom_x - (double)top_x);
+
+				// y절편 구하기
+				//y_intercept = -(line_degree * top_x);
+				
+				// 중앙점 좌표구하기
+				//one_line_center.x = (line_degree*one_line_center.y) + y_intercept;
+				if (bottom_x != -1 && top_x != -1)
+				{
+					// 중앙점의 x좌표 구하기
+					center_x = (bottom_x + top_x) / 2;
+
+					//ROS_INFO("center_x = %d", center_x);
+
+					if (pre_line_count == 1 || pre_line_count == 2)
+					{
+						pre_center_point.y = height/2;
+						pre_center_point.x = center_x;
+					}
+
+					chg_amount = pre_center_point.x - center_x;
+
+					if(abs(chg_amount) < pass_chg_amount_value)
+					{
+						degree = 0;
+					}
+					else
+					{
+						if(chg_amount > 0)
+						{
+							degree = 1;
+						}
+						else
+						{
+							degree = 0;
+						}
+						
+					}
+
+					line_count = 1;
+				}
+				//ROS_INFO("y_intercept = %f", y_intercept);
+				//ROS_INFO("one_line_center.x = %d", one_line_center.x);
+			}
+			else {}
+			//ROS_INFO("diff %d", int(diff));
+			//ROS_INFO("move_mouse_pixel %d", int(move_mouse_pixel));
+			//ROS_INFO("degree %d", int(degree));
 		}
-		
 
-		double angle_to_mid_radian = atan(x_offset / y_offset);
-		double angle_to_mid_deg = angle_to_mid_radian * 180.0 / CV_PI;
-		int steering_angle = int(angle_to_mid_deg) + 90;
+		pre_line_count = line_count;
 
-		//display heading line
-		Mat heading_image = Mat::zeros(height, width, frame.type());
-
-		double steering_angle_radian = steering_angle / 180.0 * CV_PI;
-
-		int steer_x1 = int(width / 2.0);
-		int steer_y1 = height;
-		int steer_x2 = (steer_x1 - height / 2 / tan(steering_angle_radian));
-		int steer_y2 = int(height/2);
-
-		cv::line(heading_image, Point(steer_x1, steer_y1), Point(steer_x2, steer_y2), Scalar(0, 0, 255), 5);
-
-		cv:addWeighted(view_frame, 0.8, heading_image, 1, 1, view_frame);
-
-		
-		int deviation = steering_angle - 90;
-		ROS_INFO("deviation  %d", deviation);
-
+		//ROS_INFO("count_centerline %d", count_centerline);
+		//ROS_INFO("centerline %d", centerline);
 
 		// 라인 관련 메세지(라인 각도, 라인 수)
-		traffic_state_msg.line_state = deviation;
-		traffic_state_msg.line_count = lane_lines.size();
+		//traffic_state_msg.line_state = degree;
+		traffic_state_msg.line_state = degree;
+		traffic_state_msg.line_count = line_count;
 
 
 		// ---------  주차 구역 검출  -------------
-		//ipm.applyHomography(frame, outputFrame);
-		cv::cvtColor(frame, frame_hsv, COLOR_BGR2HSV);
+		cv::cvtColor(outputFrame, frame_hsv, COLOR_BGR2HSV);
 
 		cv::inRange(frame_hsv, lower_red, upper_red, red_mask);
 
@@ -428,20 +411,31 @@ int main(int argc, char **argv)
 
 					if (approx_size % 2 == 0)
 					{
-						line(view_frame, approx[0], approx[approx.size() - 1], Scalar(0, 255, 0), 3);
+						line(red_mask, approx[0], approx[approx.size() - 1], Scalar(0, 255, 0), 3);
 
 						for (int k = 0; k < approx_size - 1; k++)
-							line(view_frame, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
+							line(red_mask, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
 
 						for (int k = 0; k < approx_size; k++)
-							circle(view_frame, approx[k], 3, Scalar(0, 0, 255));
+							circle(red_mask, approx[k], 3, Scalar(0, 0, 255));
+					}
+					else
+					{
+						line(red_mask, approx[0], approx[approx.size() - 1], Scalar(0, 255, 0), 3);
+
+						for (int k = 0; k < approx_size - 1; k++)
+							line(red_mask, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
+
+						for (int k = 0; k < approx_size; k++)
+							circle(red_mask, approx[k], 3, Scalar(0, 0, 255));
 					}
 
 					// 꼭지점의 개수가 4개일때 출력
 					if(approx_size == 4 && isContourConvex(Mat(approx)))
 					{
 						approx_size = 4;
-						ROS_INFO("station!!");
+						//ROS_INFO("rect_cont[%d][0].x %d", i, rect_cont[i][0].x);
+						//ROS_INFO("rect_cont[%d][0].y %d", i, rect_cont[i][0].y);
 					}
 					else 
 					{
@@ -476,7 +470,7 @@ int main(int argc, char **argv)
 		{
 			Point center(circles[i][0], circles[i][1]);
 			int radius = circles[i][2];
-			circle(view_frame, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+			circle(frame_light, center, radius, Scalar(0, 0, 255), 3, 8, 0);
 
 			int x1 = int(circles[i][0] - radius);
 			int y1 = int(circles[i][1] - radius);
@@ -525,9 +519,9 @@ int main(int argc, char **argv)
 					color = "green";
 					traffic_state_msg.traffic_color = GREEN;
 				}
-				putText(view_frame, color, center, CV_FONT_HERSHEY_SIMPLEX, 0.75, Scalar::all(255));
+				putText(frame_light, color, center, CV_FONT_HERSHEY_SIMPLEX, 0.75, Scalar::all(255));
 				Point center_plus_y(circles[i][0], circles[i][1] + 20);
-				putText(view_frame, to_string(mean_hue_light), center_plus_y, CV_FONT_HERSHEY_SIMPLEX, 0.75, Scalar::all(255));
+				putText(frame_light, to_string(mean_hue_light), center_plus_y, CV_FONT_HERSHEY_SIMPLEX, 0.75, Scalar::all(255));
 
 				//waitKey(1);
 			}
@@ -537,19 +531,22 @@ int main(int argc, char **argv)
 
 		//imshow("traffic_light", frame_light);
 
+		//traffic_state_msg.traffic_color = 1;
+
 		//ROS_INFO("line_state = %d", traffic_state_msg.line_state);
 		//ROS_INFO("station_area = %d", traffic_state_msg.station_area);
-		// 메세지 퍼블리싱
+		// 라인 degree 퍼블리싱
 		traffic_pub.publish(traffic_state_msg);
+		//cv::imshow("red_mask", red_mask);
 
-		cv::imshow("view_frame", view_frame);
+		//imshow("frame", frame);
 
 		if (!frame.empty())
 		{
 			// mono8 => grayscale
-			//msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", contours).toImageMsg();
+			msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", contours).toImageMsg();
 
-			msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", view_frame).toImageMsg();
+			//msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
 			image_pub.publish(msg);
 			cv::waitKey(1);
 		}
